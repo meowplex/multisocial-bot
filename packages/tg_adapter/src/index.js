fd0865e6774d4904d30821c731b40114129e5532
Trard <trardone@gmail.com>
2021-11-20 00:30:58 +0300

    style

--- packages/tg_adapter/src/index.js
+++ packages/tg_adapter/src/index.js
@@ -1,19 +1,18 @@
 import { Telegraf } from "telegraf";
-import config from "../config.js"
-import { Adapter } from "@multisocial-bot/core"
+import config from "../config.js";
+import { Adapter } from "@multisocial-bot/core";
 
 const tg = new Telegraf(config.token);
 
 tg.on("text", async (ctx) => {
     let text = ctx.message.text;
-
-    let command = Adapter.get_command(text)
+    let command = Adapter.get_command(text);
     if (command) {
-        text = Adapter.remove_trigger(text, command)
-        let params = Adapter.get_params(text, command)
-        let method_link = Adapter.get_method_link(config.server_link, command)
-        let answer = await Adapter.run(config.social_type, method_link, params)
-        ctx.reply(answer.text)
+        text = Adapter.remove_trigger(text, command);
+        let params = Adapter.get_params(text, command);
+        let method_link = Adapter.get_method_link(config.server_link, command);
+        let answer = await Adapter.run(config.social_type, method_link, params);
+        ctx.reply(answer.text);
     }
 });
 
--- packages/vk_adapter/src/index.js
+++ packages/vk_adapter/src/index.js
@@ -1,6 +1,6 @@
 import { VK } from "vk-io";
-import config from "../config.js"
-import { Adapter } from "@multisocial-bot/core"
+import config from "../config.js";
+import { Adapter } from "@multisocial-bot/core";
 
 const vk = new VK({
     token: config.token,
@@ -8,14 +8,13 @@ const vk = new VK({
 
 vk.updates.on("message_new", async (ctx) => {
     let text = ctx.text;
-
-    let command = Adapter.get_command(text)
+    let command = Adapter.get_command(text);
     if (command) {
-        text = Adapter.remove_trigger(text, command)
-        let params = Adapter.get_params(text, command)
-        let method_link = Adapter.get_method_link(config.server_link, command)
-        let answer = await Adapter.run(config.social_type, method_link, params)
-        ctx.send(answer.text)
+        text = Adapter.remove_trigger(text, command);
+        let params = Adapter.get_params(text, command);
+        let method_link = Adapter.get_method_link(config.server_link, command);
+        let answer = await Adapter.run(config.social_type, method_link, params);
+        ctx.send(answer.text);
     }
 });
 
